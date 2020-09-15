cce5b157f4e13103f06392ab2a222121a5b74ac9
alelee93 <leevargasale@gmail.com>
2020-09-14 22:07:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 let addToy = false;
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   let toyCollection = document.querySelector("#toy-collection")
   const addToyForm = toyFormContainer.firstElementChild
-  let likeButton = document.querySelectorAll(".like-btn")
+  //let likeButton = document.querySelectorAll(".like-btn")
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -22,6 +24,9 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(response => response.json())
     .then(data => createToys(data))
 
+    //let likeButtons = document.querySelectorAll(".like-btn")
+    //console.log(likeButtons)
+
 //create new tosy from array
   function createToys(data) {
     for(let i = 0; i < data.length; i++) {
@@ -32,15 +37,25 @@ document.addEventListener("DOMContentLoaded", () => {
 //render a specific toy
   function newToy(data) {
   let div = document.createElement("div")
-  
+  //console.log(data)
   div.class = "card"
   div.innerHTML = 
   `<h2> ${data.name} </h2> 
   <img src= ${data.image} class="toy-avatar" /> 
-  <p>${data.likes} </p> 
-  <button class="like-btn">Like <3</button>`
+  <p id="likes">${data.likes} </p> 
+  <button id="like-btn-${data.id}">Like <3</button>`
 
   toyCollection.appendChild(div)
+  let likeBtn = document.querySelector(`#like-btn-${data.id}`)
+
+  if(likeBtn) {
+  likeBtn.addEventListener("click", (e) => {
+    let likes = parseInt(e.target.parentElement.querySelector("#likes").innerText,10) 
+    e.target.parentElement.querySelector("#likes").innerText = likes += 1
+    //likes = likesNum + 1
+  })}
+
+  
   }
   
   //add Toy from form
@@ -80,7 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   //this is wrong, need to update !!!!!!
-  likeButton.addEventListener("click", (e) =>{
-    console.log(e.target)
-  })
+  // likeButton.addEventListener("click", (e) =>{
+  //   console.log(e.target)
+  // })
 });
